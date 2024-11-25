5375f44c6fdb3a374dff0f602c8eef4e81608da4
XGHeaven <xgheaven@gmail.com>
2024-11-26 01:30:29 +0800

    feat: use rust and add cli options (#23)

--- web/src/cases/ping.ts
+++ web/src/cases/ping.ts
@@ -1,10 +1,9 @@
 import { BASE_URL } from '../const'
 
 export const ping = async () => {
-  const resp = await fetch(`${BASE_URL}/ping`, { method: 'GET' })
   const now = performance.now()
+  const resp = await fetch(`${BASE_URL}/ping`, { method: 'GET' })
   await resp.text()
   const time = performance.now() - now
-  // rtt is twice
-  return time * 2
+  return time
 }
