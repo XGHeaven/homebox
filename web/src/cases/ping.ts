345252736369e762563e2d0a27c224078649c6fb
SukkaW <isukkaw@gmail.com>
2025-10-25 17:17:40 +0800

    refactor(web): ping use `HEAD` and `arrayBuffer`

--- web/src/cases/ping.ts
+++ web/src/cases/ping.ts
@@ -2,8 +2,13 @@ import { BASE_URL } from '../const'
 
 export const ping = async () => {
   const now = performance.now()
-  const resp = await fetch(`${BASE_URL}/ping`, { method: 'GET' })
-  await resp.text()
+  const resp = await fetch(`${BASE_URL}/ping`, { method: 'HEAD' })
+
+  // we calculate time before consuming the response
   const time = performance.now() - now
+
+  // some bad browsers that will memory leak if we don't consume the response
+  await resp.arrayBuffer()
+
   return time
 }
